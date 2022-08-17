42a810c2f0b7ec1c62d9a7ba31bfc1d6bc855147
Luiz Augusto <luizaugustodeandrades@gmail.com>
2022-08-17 15:04:00 -0300

    Criado Layout do Xadrez e exibir o tabuleiro

--- src/application/PrincipalXadrez.java
+++ src/application/PrincipalXadrez.java
@@ -1,12 +1,13 @@
 package application;
 
-import tabuleirojogo.Tabuleiro;
+import xadrez.PartidaDeXadrez;
 
 public class PrincipalXadrez {
 
 	public static void main(String[] args) {
 	
-		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+		PartidaDeXadrez partidaDeXadrez = new PartidaDeXadrez();
+		UI.printBoard(partidaDeXadrez.getPecas());
 
 	}
 
