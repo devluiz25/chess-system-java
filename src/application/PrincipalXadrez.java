fe177db6a820aee1e1e6b25b3516b84950af8cbc
Luiz Augusto <luizaugustodeandrades@gmail.com>
2022-08-19 08:33:39 -0300

    Tratando exceções e limpando a tela

--- src/application/PrincipalXadrez.java
+++ src/application/PrincipalXadrez.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import xadrez.ExcecaoXadrez;
 import xadrez.PartidaDeXadrez;
 import xadrez.PecaDeXadrez;
 import xadrez.PosicaoXadrez;
@@ -14,17 +16,27 @@ public class PrincipalXadrez {
 		PartidaDeXadrez partidaDeXadrez = new PartidaDeXadrez();
 
 		while (true) {
-			UI.printBoard(partidaDeXadrez.getPecas());
-			System.out.println();
-			System.out.print("ORIGEM: ");
-			PosicaoXadrez origem = UI.lerPosicaoXadrez(sc);
-			
-			System.out.println();
-			System.out.print("DESTINO: ");
-			PosicaoXadrez destino = UI.lerPosicaoXadrez(sc);
-			
-			PecaDeXadrez pecaCapturada = partidaDeXadrez.performanceMovimentoPeca(origem, destino);
+			try {
+				UI.limparTela();
+				UI.printBoard(partidaDeXadrez.getPecas());
+				System.out.println();
+				System.out.print("ORIGEM: ");
+				PosicaoXadrez origem = UI.lerPosicaoXadrez(sc);
 
+				System.out.println();
+				System.out.print("DESTINO: ");
+				PosicaoXadrez destino = UI.lerPosicaoXadrez(sc);
+
+				PecaDeXadrez pecaCapturada = partidaDeXadrez.performanceMovimentoPeca(origem, destino);
+			} 
+			catch (ExcecaoXadrez e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch(InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 	}
 
--- src/application/UI.java
+++ src/application/UI.java
@@ -27,14 +27,18 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	public static void limparTela() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
 	public static PosicaoXadrez lerPosicaoXadrez(Scanner sc) {
 		try {
 			String s = sc.nextLine();
 			char coluna = s.charAt(0);
 			int linha = Integer.parseInt(s.substring(1));
 			return new PosicaoXadrez(coluna, linha);
-		} 
-		catch (RuntimeException e) {
+		} catch (RuntimeException e) {
 			throw new InputMismatchException("ERRO NA LEITURA DA POSICAO DAS PECAS: VALORES VALIDOS SAO DE A1 A H8!");
 		}
 	}
